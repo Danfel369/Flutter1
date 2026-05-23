8124ebbfbf988ebfc2982717df4b7abbd0129f20
Danfel369 <danielfelipecreyes@gmail.com>
2026-05-22 22:54:46 -0500

    Se edita plantilla anterior para crear tarjeta de presentación

--- lib/principal.dart
+++ lib/principal.dart
@@ -11,24 +11,22 @@ class MiBombo extends StatelessWidget {
       theme: ThemeData(colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple)),
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Saluditos Page'),
+          title: const Text('Tarjeta de Información'),
       ),
       body: Center(
         child: Container(
-          width: 300,
+          width: 500,
           height: 300,
 
           //espacio del contenedor
           margin: const EdgeInsets.all(20),
 
           decoration: BoxDecoration(
-            color: Colors.deepPurple.shade500,
+            color: Colors.amber,
             borderRadius: BorderRadius.circular(20),
           ),
 
         child: Row(
-
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
 
           children: [
             Column(
@@ -39,36 +37,87 @@ class MiBombo extends StatelessWidget {
                 //hijo 1
                 Icon(
                   Icons.person,
-                  color: const Color.fromARGB(255, 209, 194, 194),
-                  size: 50,
+                  color: Colors.cyan,
+                  size: 100,
                 ),
                 //hijo 2 
-                Text(
-                  'Persona',
-                  style: TextStyle(
-                    color: const Color.fromARGB(255, 201, 27, 27),
-                    fontSize: 20,
-                  ),
-                )
               ],
             ),
             //hijo 2 de row
             Column(
               //centrar elementos de la columna
-              mainAxisAlignment: MainAxisAlignment.center,
+
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Icon(
-                  Icons.phone_android,
-                  color: Colors.black,
-                  size: 50,
-                ),
+                //Fila hija
                 Text(
-                  'Sistema operativo',
+                  'Daniel C',
                   style: TextStyle(
                     color: Colors.black,
-                    fontSize: 20,
+                    fontSize: 30,
                   ),
-                )
+                ),
+                Row(
+                  //hijos contenedor
+                  children: [
+                    Column(
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      //hijos de esta columna
+                      children: [
+                        Row(
+                          //hijos de esta fila
+                          children: [
+                            Icon(
+                              Icons.email,
+                              color:  Colors.black,
+                              size:20,
+                            ),
+                            Text(
+                              'danfelkings@gmail.com',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                              ),
+                            )
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.phone,
+                              color: Colors.black,
+                              size: 20,
+                            ),
+                            Text(
+                              '314356825',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                              ),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Icon(
+                              Icons.location_on,
+                              color: Colors.black,
+                              size: 20,
+                            ),
+                            Text(
+                              'Bogotá',
+                              style: TextStyle(
+                                color: Colors.black,
+                                fontSize: 20,
+                              ),
+                            )
+                          ],
+                        )
+                      ]
+                    )
+                  ],
+                ),
+                
               ]
             ),
           ],

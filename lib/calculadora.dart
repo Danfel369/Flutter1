7a2fc97a5edb05e0b9072139c75cdfbdcfa4dd76
Danfel369 <danielfelipecreyes@gmail.com>
2026-06-04 22:37:38 -0500

    Se solucionan errores anteriores

--- lib/calculadora.dart
+++ lib/calculadora.dart
@@ -1,7 +1,19 @@
+import 'dart:nativewrappers/_internal/vm/lib/ffi_patch.dart';
+
 import 'package:flutter/material.dart';
 
-class Calculadora extends StatelessWidget {
-  const Calculadora({super.key});
+class Calculadora extends StatefulWidget {
+  const Calculadora ({super.key});
+  
+  @override
+  State<Calculadora> createState() => _CalculadoraState();
+}
+
+class _CalculadoraState extends State<Calculadora>{
+  String operacion = "";
+  String resultado = "14";  
+  Array elements = [ ];
+
 
   @override
   Widget build(BuildContext context) {
@@ -18,19 +30,22 @@ class Calculadora extends StatelessWidget {
                 child: Column(
                   mainAxisAlignment: MainAxisAlignment.end,
                   crossAxisAlignment: CrossAxisAlignment.end,
-                  children: const [
+                  children: [
                     Text(
-                      '8 + 15',
+                      operacion,
                       style: TextStyle(color: Colors.white, fontSize: 35),
                     ),
                     SizedBox(height: 10),
                     Text(
-                      '23',
+                      resultado,
                       style: TextStyle(color: Colors.white, fontSize: 50),
                     ),
                   ],
                 ),
               ),
+            ),
+            Row(
+              children: [boton('CA'), boton('CE'), boton('%'), boton('<-') ],
             ),
             Row(
               children: [boton('7'), boton('8'), boton('9'), boton('/') ],
@@ -39,20 +54,52 @@ class Calculadora extends StatelessWidget {
               children: [boton('4'), boton('5'), boton('6'), boton('*') ],
             ),
             Row(
-              children: [boton('1'), boton('2'), boton('3'), boton('+') ],
+              children: [boton('1'), boton('2'), boton('3'), boton('-') ],
             ),
             Row(
-              children: [boton('0'), boton('=') ],
+              children: [boton(','), boton('0'), boton('='), boton('+') ],
             ),
           ],
         ),
       ),
     );
+  } //build
+
+  void actionBoton(String value) {
+    //print(valor);
+    setState(() {
+      operacion += value;
+    });
+    switch (value) {
+      case 'CA': 
+        operacion = "";
+        resultado = "0";
+        return;
+      case 'CE':
+        operacion = "";
+        return;
+      case '<-':
+        
+        return;
+        case "+":
+        case "-":
+        case "*":
+        case "/":
+
+      break;
+
+      default:  
+      operacion += value;
+      actualNum += value;
+    }
   }
 
   Widget boton(String texto) {
     return Expanded(
-      child: ElevatedButton(onPressed: () {}, child: Text(texto)),
-      );
+      child: ElevatedButton(onPressed: () {
+        actionBoton(texto);
+      }, 
+      child: Text(texto)),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:pimerproyectico/calculadora.dart';
-import 'package:pimerproyectico/formulario.dart';
-import 'package:pimerproyectico/principal.dart';
-import 'package:pimerproyectico/rutas.dart';
+
 
 void main() {
   runApp(
     const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Calculadora(),
-    )
+      home: Calculadora())
   );
 }
